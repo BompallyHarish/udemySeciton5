9a0c4b1bc211deb51fe259de4c91564d10d13537
Harish Bompally <harishb@zenwork.com>
2024-05-08 10:08:40 +0530

    Second commit .

--- app/src/app/app.component.ts
+++ app/src/app/app.component.ts
@@ -7,7 +7,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
-  serverElements = [];
+  serverElements = [{type: "server", name: "Test Server", content:"This is just a test server!" }];
 
 
 
--- app/src/app/server-element/server-element.component.ts
+++ app/src/app/server-element/server-element.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Input } from '@angular/core';
 
 @Component({
   selector: 'app-server-element',
@@ -6,5 +7,5 @@ import { Component } from '@angular/core';
   styleUrl: './server-element.component.css'
 })
 export class ServerElementComponent {
-
+  @Input('srvElement') element: { type: String, name: String, content: String }
 }
